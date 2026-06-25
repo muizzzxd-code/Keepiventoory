74ec365d7a6f6bba4d7849556333fc846fe84240
muizzzxd-code <muizzzxd@gmail.com>
2026-06-25 15:06:28 +0700

    Create KeepInventoryMod.java

---
+++ src/main/java/com/keepinventory/mod/KeepInventoryMod.java
@@ -0,0 +1,16 @@
+package com.keepinventory.mod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class KeepInventoryMod implements ModInitializer {
+
+    public static final String MOD_ID = "keepinventory";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+        LOGGER.info("[KeepInventory] Mod aktif! Item tidak akan jatuh saat mati.");
+    }
+}
